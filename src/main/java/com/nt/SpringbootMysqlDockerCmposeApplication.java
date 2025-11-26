634065d7cd30518095f39040fef994097170dbba
Bluepal-rakesh-swain <rakesh.swain@bluepal.com>
2025-11-26 12:48:31 +0530

    first commit

---
+++ src/main/java/com/nt/SpringbootMysqlDockerCmposeApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMysqlDockerCmposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMysqlDockerCmposeApplication.class, args);
+	}
+
+}
